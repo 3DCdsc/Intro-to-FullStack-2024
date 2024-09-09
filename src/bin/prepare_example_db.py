740c479b280fa626e7666512e124108a3c74e611
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-09-09 22:29:17 +0800

    feat: incomplete script to prep example database

---
+++ src/bin/prepare_example_db.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+from pathlib import Path
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    from src import Base
+
+    engine = create_engine("sqlite:///src/example.db")
+
+    Session = sessionmaker(bind=engine)
+
+    with Session.begin() as sess:
+        Base.metadata.create_all(sess.bind)
+
+    # TODO: The rest of the example data setup.
+
+
+if __name__ == "__main__":
+    # Tomfoolery to allow importing src although its not a proper package.
+    sys.path.append(str((Path(__file__) / ".." / ".." / "..").resolve()))
+    main()
